e74f55a87e17fe535f9490d26c812000056f1321
JulieKodehode <julie.kodehode@gmail.com>
2023-01-20 14:08:40 +0100

    Finished.

--- src/AdviceGeneratorApp.jsx
+++ src/AdviceGeneratorApp.jsx
@@ -1,3 +1,6 @@
+// Libraries
+import { useState, useEffect } from "react";
+
 // Styles
 import "./styles/styles.css";
 import "./styles/colors.css";
@@ -7,15 +10,41 @@ import "./styles/typography.css";
 import Divider from "./components/Divider/Divider";
 
 const AdvanceGeneratorApp = () => {
+	const [adviceData, setAdviceData] = useState([]);
+	const getAdvice = () => {
+		fetch("https://api.adviceslip.com/advice")
+			.then((response) => response.json())
+			.then((adviceData) => {
+				console.log(adviceData);
+				console.log(adviceData.slip);
+				console.log(adviceData.slip.advice);
+				console.log(adviceData.slip.id);
+
+				setAdviceData(adviceData.slip);
+			})
+			// Catches errors
+			.catch((error) => {
+				console.log(error.message);
+			});
+	};
+
+	// useEffect to only fetch once
+	useEffect(() => {
+		getAdvice();
+	}, []);
+
+	// Handleclick on button to run the getAdvice function again (new advice)
+	const handleClick = () => {
+		getAdvice();
+	};
+
 	return (
 		<main className="adviceMain">
 			<div className="adviceCard">
-				<p className="adviceNumber">Advice number 001</p>
-				<p className="adviceText">
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, consequuntur!
-				</p>
+				<h1 className="adviceNumber">Advice #{adviceData.id}</h1>
+				<h4 className="adviceText">"{adviceData.advice}"</h4>
 				<Divider />
-				<button className="nextAdvice"></button>
+				<button onClick={handleClick} className="nextAdvice"></button>
 			</div>
 		</main>
 	);
